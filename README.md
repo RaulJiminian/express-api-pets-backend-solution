b31fd6afdd87ef45ff49c2ca1df962e181b18824
RaulJiminian <raul.e.jiminian@gmail.com>
2026-02-03 15:55:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pets Backend
+
+- Fork/Clone repo
+- Create a .gitignore and .env file
+- Add your MONGODB_URI to the .env file
---
+++ models/pet.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const petSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, min: 0, required: true },
+  breed: { type: String },
+});
+
+export default mongoose.model("Pet", petSchema);
